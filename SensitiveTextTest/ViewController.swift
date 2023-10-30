5a868412d70d4c770462127783ea2fef8d91b7e6
Ricky Chen <rickychen@ddim.com.tw>
2023-10-30 18:57:38 +0800

    Initial Commit

---
+++ SensitiveTextTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SensitiveTextTest
+//
+//  Created by 陳耕霈 on 2023/10/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
